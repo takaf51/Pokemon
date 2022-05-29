211bc2c98a8e14602cd6f82bc38a1599552df218
Taka <takapohny@gmail.com>
2022-05-29 22:23:01 +0000

    Solved: Warning: Use the `defaultValue` or `value` props on <select> instead of setting `selected` on <option>

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Select = props => {
   const {pokeList, onSelect, curIndex} = props;
-  const content = pokeList.map((p,index) => <option key={index}  value={index} {...(index === curIndex ? { selected: 1 } : {})}>{`${index + 1}: ${p.name}`}</option>)
+  const content = pokeList.map((p,index) => <option key={index}  value={index} >{`${index + 1}: ${p.name}`}</option>)
   return (
-    <select className='w-full mb-2 border-1 rounded-md h-[50px] text-[22px]' onChange={e => onSelect.call(null, e.target.value)}>
+    <select value={curIndex} className='w-full mb-2 border-1 rounded-md h-[50px] text-[22px]' onChange={e => onSelect.call(null, e.target.value)}>
       {content}
     </select>
   );
